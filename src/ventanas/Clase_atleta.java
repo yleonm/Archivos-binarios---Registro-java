a1c35594e17ab6b60ed22ad930a6d4228874f579
Yerson León <yelemo777@gmail.com>
2021-01-17 20:18:16 -0500

    Creacion objeto atleta dentro de la clase atleta

--- src/ventanas/Clase_atleta.java
+++ src/ventanas/Clase_atleta.java
@@ -1,14 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ventanas;
 
-/**
- *
- * @author H.P
- */
-public class Clase_atleta {
+import java.io.Serializable;
+
+
+public class Clase_atleta implements Serializable{
+    private String nombre;
+    private int edad;
+    private double peso;
+    private int estatura;
+
+    public Clase_atleta(String nombre, int edad, double peso, int estatura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.estatura = estatura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public int getEstatura() {
+        return estatura;
+    }
+    
     
 }
